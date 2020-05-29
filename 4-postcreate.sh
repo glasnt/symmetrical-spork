d800ff1d18caede1ead1e656e377f1116ce65f16
Katie McLaughlin <katie@glasnt.com>
2020-05-29 12:37:44 +1000

    secret test

--- 4-postcreate.sh
+++ 4-postcreate.sh
@@ -1,4 +1,1 @@
 echo "This is the fourth script, fired at post-create."
-gcloud run services update ${K_SERVICE} --project ${GOOGLE_CLOUD_PROJECT} --region ${GOOGLE_CLOUD_REGION} \
-    --set-env-vars TARGET=${TARGET} \
-    --set-env-vars EMOJI=${EMOJI} --platform managed
